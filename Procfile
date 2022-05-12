81bf585290b48df9f0bd370a1d1de8ecfbfdbeb7
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2022-05-12 18:11:11 +0530

    initialzing

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT knowledge_pro.wsgi:application
--- ccgc/settings.py
+++ ccgc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fr_tz@64jv&nuz7hq$9w=6_d%tqilr-7dp21@9ammfll3qsi^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +123,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    (os.path.join(BASE_DIR, 'static'))
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
