cf5b7af0b285537f23d705f17556b85a8b2fb5a8
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2022-10-20 13:43:13 +0530

    db

--- ccgc/settings.py
+++ ccgc/settings.py
@@ -77,17 +77,6 @@ WSGI_APPLICATION = 'ccgc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'demo_ccagc',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Abrardar123',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
